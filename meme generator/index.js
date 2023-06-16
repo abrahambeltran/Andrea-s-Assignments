b3c4607d5207fb441c356d82791a1e7457895324
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-06-16 12:25:23 -0400

    Partially functional meme gen

--- meme generator/index.js
+++ meme generator/index.js
@@ -1,9 +1,32 @@
-const paragraph = document.querySelector('p');
+function memeMaker(event) {
+    event.preventDefault();
+    const imageUrl = document.getElementById('imageInput').value;
+    const topText = document.getElementById('topTextInput').value;
+    const bottomText = document.getElementById('bottomTextInput').value;
 
-paragraph.addEventListener("click", updateName);
+    const memeContainer = document.createElement("div");
+    memeContainer.classList.add("meme");
 
-function updateName(){
-    const name = prompt("NEW NAME");
-    paragraph.textContent = `PLAYER 1: ${name}`;
-    //document.body.appendChild();
+    const memeImage = document.createElement("img");
+    memeImage.src = imageUrl;
+
+    const topTextElement = document.createElement("div");
+    topTextElement.classList.add('top-text');
+    topTextElement.innerText = topText;
+
+    const bottomTextElement = document.createElement("div");
+    bottomTextElement.classList.add('bottom-text');
+    bottomTextElement.innerText = bottomText;
+
+    memeContainer.appendChild(memeImage);
+    memeContainer.appendChild(bottomTextElement);
+    memeContainer.appendChild(topTextElement);
+
+    document.getElementById('memeContainer').appendChild(memeContainer);
+
+    document.getElementById('imageInput').value = '';
+    document.getElementById('topTextInput').value = '';
+    document.getElementById('bottomTextInput').value = '';
 }
+
+document.getElementById('memeForm').addEventListener("submit", memeMaker);
